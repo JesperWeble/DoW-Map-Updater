beea7d82899df0baf8b44be0191ed446244389a5
Fairy-Fair <72870620+Fairy-Fair@users.noreply.github.com>
2025-04-30 18:12:43 +0200

    using bitsadmin and tar instead of powershell.

--- mapUpdater.bat
+++ mapUpdater.bat
@@ -6,16 +6,17 @@ setlocal
 set "ZIP_URL=https://github.com/JesperWeble/DoW-Map-Updater/archive/refs/heads/main.zip"
 set "TEMP_ZIP=%~dp0mapUpdaterTEMP.zip"
 set "EXTRACTED_FOLDER=%~dp0mapUpdaterTEMP"
-set "TARGET_FOLDER=%~dp0DXP2"
+set "TARGET_FOLDER=%~dp0DoW-Map-Updater-main\DXP2"
 
 :: Download
-
-powershell -Command "Invoke-WebRequest -Uri '%ZIP_URL%' -OutFile '%TEMP_ZIP%'"
+bitsadmin /transfer downloadOperation /download /priority high "%ZIP_URL%" "%TEMP_ZIP%"
+@REM powershell -Command "Invoke-WebRequest -Uri '%ZIP_URL%' -OutFile '%TEMP_ZIP%'"
 if exist "%EXTRACTED_FOLDER%" rmdir /s /q "%EXTRACTED_FOLDER%"
 
 
 :: Extract
-powershell -Command "Expand-Archive -Path '%TEMP_ZIP%' -DestinationPath '%EXTRACTED_FOLDER%'"
+tar -xf "%TEMP_ZIP%" -C "%EXTRACTED_FOLDER%"
+@REM powershell -Command "Expand-Archive -Path '%TEMP_ZIP%' -DestinationPath '%EXTRACTED_FOLDER%'"
 xcopy "%EXCTRACTED_FOLDER%\DXP2\*" "%TARGET_FOLDER%\" /s /y
 
 :: Cleanup
