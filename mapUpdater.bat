ab4b6c899762b7e4c660c33f35d6dad5e315691d
Fairy-Fair <72870620+Fairy-Fair@users.noreply.github.com>
2025-04-30 17:59:33 +0200

    Removed leftover backup code.

--- mapUpdater.bat
+++ mapUpdater.bat
@@ -24,38 +24,3 @@ rmdir /s /q "%EXTRACTED_FOLDER%"
 
 echo Update Complete!
 pause
-
-
-
-
-
-
-
-
-@REM @echo off
-@REM setlocal
-
-@REM :: Set working paths
-@REM set "ZIP_URL=URL for zip download here"
-@REM set "TEMP_ZIP=%~dp0temp.zip"
-@REM set "EXTRACTED_FOLDER=%~dp0extracted"
-@REM set "TARGET_FOLDER=%~dp0URL for local folder name here"
-
-@REM :: Download the zip file
-@REM powershell -Command "Invoke-WebRequest -Uri '%ZIP_URL%' -OutFile '%TEMP_ZIP%'"
-
-@REM :: Remove old extracted folder if it exists
-@REM if exist "%EXTRACTED_FOLDER%" rmdir /s /q "%EXTRACTED_FOLDER%"
-
-@REM :: Extract zip
-@REM powershell -Command "Expand-Archive -Path '%TEMP_ZIP%' -DestinationPath '%EXTRACTED_FOLDER%'"
-
-@REM :: Copy new files to target folder
-@REM xcopy "%EXTRACTED_FOLDER%\URL for folder inside zip here\*" "%TARGET_FOLDER%\" /s /e /y
-
-@REM :: Cleanup
-@REM del "%TEMP_ZIP%"
-@REM rmdir /s /q "%EXTRACTED_FOLDER%"
-
-@REM echo Done.
-@REM pause
