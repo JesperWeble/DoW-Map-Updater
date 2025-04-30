e691606b0f0ee73e83953b7533583083cfcaa55e
Fairy-Fair <72870620+Fairy-Fair@users.noreply.github.com>
2025-04-30 19:52:46 +0200

    Clean up and more echoes.

--- mapUpdater.bat
+++ mapUpdater.bat
@@ -9,21 +9,18 @@ set "TEMP_ZIP=%~dp0%TEMP%.zip"
 set "EXTRACTED_FOLDER=%~dp0mapUpdaterTEMP"
 set "TARGET_FOLDER=%~dp0\DXP2"
 
-
-
-
-
 :: Download
 certutil -urlcache -split -f "%ZIP_URL%" "%TEMP_ZIP%" >nul
 if not exist "%TEMP_ZIP%" (
     echo ERROR: file was not downloaded.
     pause
 )
-echo %TEMP%.zip exists as DoW-Map-Updater-main.zip
+echo %TEMP%.zip exists
 if not exist "%EXTRACTED_FOLDER%" mkdir "%EXTRACTED_FOLDER%"
 
 :: Extract
 tar -xf "%TEMP_ZIP%" -C "%EXTRACTED_FOLDER%" >nul
+echo extraction complete.
 
 :: Update this file
 copy "%EXTRACTED_FOLDER%\%TEMP%\mapUpdater.bat" "%~dp0" /y
