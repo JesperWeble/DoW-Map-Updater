142dd45c8091a323cbcc8f39d1e6d0fde89a9b3f
Fairy-Fair <72870620+Fairy-Fair@users.noreply.github.com>
2025-04-30 18:44:40 +0200

    Removed leftover powershell stuff added an error catch.

--- mapUpdater.bat
+++ mapUpdater.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 
+
 :: Paths
 :: %~dp0 means "Current Directory" i.e the directory of the .bat file.
 set "ZIP_URL=https://github.com/JesperWeble/DoW-Map-Updater/archive/refs/heads/main.zip"
@@ -10,13 +11,14 @@ set "TARGET_FOLDER=%~dp0DoW-Map-Updater-main\DXP2"
 
 :: Download
 bitsadmin /transfer downloadOperation /download /priority high "%ZIP_URL%" "%TEMP_ZIP%"
-@REM powershell -Command "Invoke-WebRequest -Uri '%ZIP_URL%' -OutFile '%TEMP_ZIP%'"
 if exist "%EXTRACTED_FOLDER%" rmdir /s /q "%EXTRACTED_FOLDER%"
-
+if not exist "%TEMP_ZIP%" (
+    echo ERROR: file was not downloaded.
+    pause
+)
 
 :: Extract
 tar -xf "%TEMP_ZIP%" -C "%EXTRACTED_FOLDER%"
-@REM powershell -Command "Expand-Archive -Path '%TEMP_ZIP%' -DestinationPath '%EXTRACTED_FOLDER%'"
 xcopy "%EXCTRACTED_FOLDER%\DXP2\*" "%TARGET_FOLDER%\" /s /y
 
 :: Cleanup
