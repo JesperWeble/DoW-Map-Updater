731f3e0698fb73ac791a024d2f8d4310f0430b63
Fairy-Fair <72870620+Fairy-Fair@users.noreply.github.com>
2025-04-30 20:38:18 +0200

    Adds a folder for the maps and removes it if it already exists to effectively update the maps rather than only add.
    New maps added: Cathedral of Recalcitrants 2p edits.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DoW-Map-Updater
 A simple program that makes sharing and updating custom Warhammer 40.000 Dawn of War map files with my brother.
+
+
+Læg denne fil ind i din Dawn of War folder (der hvor Mod Manageren og Soulstorm.exe også lægger)
+
+Når du køre denne fil så downloader den automatisk alle de maps som jeg har ind i dit spil så vi ikke behøver at share dem over discord hver gang jeg ændre på noget.
+Derudover så opdatere denne fil sig også til den nyeste version når du køre den!
--- mapUpdater.bat
+++ mapUpdater.bat
@@ -1,6 +1,9 @@
 @echo off
 setlocal
+
+:: Ensures that the update does not loop.
 if "%~1" == "updated" exit
+
 :: Paths
 :: %~dp0 means "Current Directory" i.e the directory of the .bat file.
 set "ZIP_URL=https://github.com/JesperWeble/DoW-Map-Updater/archive/refs/heads/main.zip"
@@ -8,6 +11,7 @@ set "TEMP=DoW-Map-Updater-main"
 set "TEMP_ZIP=%~dp0%TEMP%.zip"
 set "EXTRACTED_FOLDER=%~dp0mapUpdaterTEMP"
 set "TARGET_FOLDER=%~dp0\DXP2"
+set "OUR_SHARED_MAPS=%TARGET_FOLDER%\Data\Scenarios\mp\Our-Shared-Maps"
 
 :: Download
 certutil -urlcache -split -f "%ZIP_URL%" "%TEMP_ZIP%" >nul
@@ -22,22 +26,19 @@ if not exist "%EXTRACTED_FOLDER%" mkdir "%EXTRACTED_FOLDER%"
 tar -xf "%TEMP_ZIP%" -C "%EXTRACTED_FOLDER%" >nul
 echo extraction complete.
 
+:: Share the maps
+if exist "%OUR_SHARED_MAPS%" rmdir /s /q "%OUR_SHARED_MAPS%"
+xcopy "%EXTRACTED_FOLDER%\%TEMP%\DXP2\*" "%TARGET_FOLDER%\" /s /y
+
 :: Update this file
 copy "%EXTRACTED_FOLDER%\%TEMP%\mapUpdater.bat" "%~dp0" /y
-
-
-xcopy "%EXTRACTED_FOLDER%\%TEMP%\DXP2\*" "%TARGET_FOLDER%\" /s /y
 
 :: Cleanup
 del "%TEMP_ZIP%"
 rmdir /s /q "%EXTRACTED_FOLDER%"
 
+:: Run the newest version of this file.
 start "" "%~dp0mapUpdater.bat" updated
 
 echo Update Complete!
 pause
-
-
-
-
-@REM bitsadmin /transfer downloadOperation /download /priority high "%ZIP_URL%" "%TEMP_ZIP%"
